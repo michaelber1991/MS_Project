05296ee50a9c504a6178fd06ac2e0dfe2e0fa5c1
michaelber1991 <michaelber1991@hotmail.com>
2025-03-25 20:54:40 +0100

    [Config]: init project

---
+++ MS_Project/Controllers/WeatherController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MS_Project.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherController : ControllerBase
+{
+    private static readonly string[] Summaries =
+    [
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public ActionResult<IEnumerable<WeatherForecast>> Get()
+    {
+        try
+        {
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    (
+                        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        Random.Shared.Next(-20, 55),
+                        Summaries[Random.Shared.Next(Summaries.Length)]
+                    ))
+                .ToArray();
+
+            return Ok(forecast);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "An error occurred: " + ex.Message);
+        }
+    }
+}
+
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
